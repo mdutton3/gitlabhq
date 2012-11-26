5efb4870a4a9bdd020adec196dd7a6808cac07bb
Matthew Dutton <matt.dutton@motionreality.com>
2012-11-26 18:58:21 -0500

    Setup basic SMTP parameters for email notifications

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,12 +65,17 @@ Gitlab::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
-  config.action_mailer.delivery_method = :sendmail
-  # Defaults to:
-  # # config.action_mailer.sendmail_settings = {
-  # #   :location => '/usr/sbin/sendmail',
-  # #   :arguments => '-i -t'
-  # # }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :enable_starttls_auto => true,
+    :address            => "smtp.gmail.com",
+    :port               => "587",
+    :domain             => "motionreality.com",
+    :authentication     => :plain,
+    :user_name          => "gitlab-notify@motionreality.com",
+    :password           => "password"
+  }
+
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
 end
